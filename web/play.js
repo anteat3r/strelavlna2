2183a0567bbf988c44ab3a2cd835fcdc76d5afed
Dremer3135 <114572789+Dremer3135@users.noreply.github.com>
2024-09-18 20:46:33 +0200

    play stranka

---
+++ web/play.js
@@ -0,0 +1,13 @@
+// const buy_button_wrapper = document.getElementById("buy-button-wrapper");
+// const buy_button = document.getElementById("buy-button");
+
+
+// buy_button.addEventListener("click", function(){
+//     if(buy_button_wrapper.classList.contains("buy-button-close")){
+//         buy_button_wrapper.classList.add("buy-button-open");
+//         buy_button_wrapper.classList.remove("buy-button-close");
+//     }else{
+//         buy_button_wrapper.classList.add("buy-button-close");
+//         buy_button_wrapper.classList.remove("buy-button-open");
+//     }
+// })
--- web/verify_registration.js
+++ web/verify_registration.js
@@ -9,23 +9,24 @@ const infoT = document.getElementById("register-verification-title");
 window.addEventListener("load", () => {
     console.log(id);
     is_loading_running = true;
+    animation_color = "#3118ba"
 
     fetch(`https://strela-vlna.gchd.cz/api/regconfirm/${id}`, {
         method: 'GET',
     })
     .then(response => {
+        if (response.ok) {
+            infoT.innerHTML = "Hotovo!";
+            infoP.innerHTML = "Registrace proběhla úspěšně";
+        } else {
+            infoT.innerHTML = "Chyba!";
+            response.text().then(e =>{
+                infoP.innerHTML = e;
+            });
+        }
         setTimeout(() => {
             is_loading_running = false;
-            if (response.ok) {
-                infoT.innerHTML = "Hotovo!";
-                infoP.innerHTML = "Registrace proběhla úspěšně";
-            } else {
-                infoT.innerHTML = "Chyba!";
-                response.text().then(e =>{
-                    infoP.innerHTML = e;
-                });
-            }
-        }, 1500);
+        }, 1000);
 
 
         
