895a6c7c42f7f4b7dda91ecb81cd2a41d40320b9
Dremer3135 <114572789+Dremer3135@users.noreply.github.com>
2024-09-19 19:29:02 +0200

    play update

--- web/play.js
+++ web/play.js
@@ -1,6 +1,77 @@
+//global states
+var team_balance = 25;
+var team_name = "Team 1";
+var team_rank = "14";
+var end_time = new Date().getTime() + 100000;
+var problems = [{
+    id: "askjdhiwuahskjd",
+    title: "Lyzar ve vesnici co si koupil moc drahy listky",
+    rank: "A",
+    worked_on: true,
+    last_answer_time: new Date().getTime(),
+    seen_chat: true,
+    problem_content: "Mlékárna vykoupí od zemědělců mléko jedině tehdy, má-li předepsanou teplotu 4°C. Farmář při kontrolním měření zjistil, že jeho 60 litrů mléka má teplotu jen 3,6°C. Pomůže mu 10 litrů mléka o teplotě 6,5°C, které původně zamýšlel uschovat pro potřeby své rodiny? Zbude mu nějaké mléko aspoň na snídani? Anebo mu mlékárna mléko vůbec nevykoupí?",
+    chat: [
+        {
+            author: "support",
+            content: "akjsd kjashdkjhi udawhkjs duiwoah sldjl jaw diajsdl jaoiw jlasjd lkj"
+        },
+        {
+            author: "support",
+            content: "akjsd kjaajsdl jaoiw jlasjd lkj"
+        },
+        {
+            author: "team",
+            content: "docela noobatik"
+        },
+        {
+            author: "support",
+            content: "akjsd kjashdkjhi udawhkjs duiwoah sldjl jaw diajsdl jaoiw jlasjd lkj"
+        },
+    ]
+},
+{
+    id: "bnvcxiuwalknsdlkj",
+    title: "Pecka u malého nuubatika",
+    rank: "C",
+    worked_on: false,
+    last_answer_time: Date.now(),
+    seen_chat: false,
+    problem_content: "asi Mlékárna vykoupí od zemědělců mléko jedině tehdy, má-li předepsanou teplotu 4°C. Farmář při kontrolním měření zjistil, že jeho 60 litrů mléka má teplotu jen 3,6°C. Pomůže mu 10 litrů mléka o teplotě 6,5°C, které původně zamýšlel uschovat pro potřeby své rodiny? Zbude mu nějaké mléko aspoň na snídani? Anebo mu mlékárna mléko vůbec nevykoupí?",
+    chat: [
+        {
+            author: "support",
+            content: "akjsd kjaajsdl jaoiw jlasjd lkj"
+        },
+        {
+            author: "team",
+            content: "docela noobatik"
+        },
+        {
+            author: "support",
+            content: "akjsd kjashdkjhi udawhkjs duiwoah sldjl jaw diajsdl jaoiw jlasjd lkj"
+        },
+    ]
+}
+];
+
+//local states
+var focused_problem = "askjdhiwuahskjd";
+
+
+
+//initial setup
+updateTeamStats();
+updateProblemList();
+updateFocusedProblem()
+
+
+//buy events
 const buy_button_wrapper = document.getElementById("buy-button-wrapper");
 const buy_button = document.getElementById("buy-button");
-
+const easy_buy = document.getElementById("easy-buy");
+const medium_buy = document.getElementById("medium-buy");
+const hard_buy = document.getElementById("hard-buy");
 
 buy_button.addEventListener("click", function(){
     if(buy_button_wrapper.classList.contains("buy-button-close")){
@@ -11,3 +82,136 @@ buy_button.addEventListener("click", function(){
         buy_button_wrapper.classList.remove("buy-button-open");
     }
 })
+
+
+function updateTeamStats(){
+    document.getElementById("team-name").innerHTML = team_name;
+    document.getElementById("rank").innerHTML = team_rank;
+    document.getElementById("team-balance").innerHTML = team_balance + " DC"
+}
+
+function updateProblemList(){
+    const problem_wrapper = document.getElementById("teams-problems");
+    problem_wrapper.innerHTML = "";
+    for(prob of problems){
+        problem_wrapper.innerHTML +=
+        `<div class="problem ${focused_problem == prob.id ? "problem-focused" : ""} ${prob.worked_on ? "problem-worked-on" : ""} ${!prob.seen_chat ? "unseen-chat" : ""}" id="${prob.id}">
+            <div class="flex flex-row align-center">
+                <h2 class="problem-title"${prob.title.length > 12 ? `style="font-size: 15px"` : ""}>${prob.title}</h2>
+                <h2 class="problem-rank">[${prob.rank}]</h2>
+            </div>
+            <div class="occupied-icon"></div>
+        </div>`
+    }
+}
+
+function updateFocusedProblem(){
+    if(focused_problem == "" || !problems.some(prob => prob.id == focused_problem)){
+        focused_problem = "";
+
+
+        return;
+    }
+    const problem_title = document.getElementById("problem-title");
+    const problem_content = document.getElementById("problem-content");
+    const sell_information = document.getElementById("sell-information");
+    const focused_problem_obj = problems.find(prob => prob.id == focused_problem);
+    problem_title.innerHTML = focused_problem_obj.title + " [" + focused_problem_obj.rank + "]";
+    problem_content.innerHTML = focused_problem_obj.problem_content;
+    sell_information.innerHTML = `*Prodat aktuální úlohu: <span class="bold">${focused_problem_obj.title}</span>`;
+}
+
+
+
+
+
+lastSecond = 0;
+function update(){
+    requestAnimationFrame(update);
+
+    //clock
+    const now = new Date().getTime();
+    const remaining = end_time - now;
+    if (Math.floor(remaining / 1000) != lastSecond && remaining>=0){
+        lastSecond = Math.floor(remaining / 1000);
+
+        const hours = Math.floor(remaining / 1000 / 60 / 60) % 24;
+        const minutes = Math.floor(remaining / 1000 / 60) % 60 % 10;
+        const seconds = Math.floor(remaining / 1000) % 60 % 10;
+        const ten_seconds = Math.floor(Math.floor(remaining / 1000) % 60 / 10);
+        const ten_minutes = Math.floor(Math.floor(remaining / 1000 / 60) % 60 / 10);
+    
+        const hour_wrapper = document.getElementById("hours").getElementsByClassName("digit-wrapper")[0];
+        const ten_minute_wrapper = document.getElementById("ten-minutes").getElementsByClassName("digit-wrapper")[0];
+        const minute_wrapper = document.getElementById("minutes").getElementsByClassName("digit-wrapper")[0];
+        const ten_second_wrapper = document.getElementById("ten-seconds").getElementsByClassName("digit-wrapper")[0];
+        const second_wrapper = document.getElementById("seconds").getElementsByClassName("digit-wrapper")[0];
+    
+        for(let i = 1; i < 10; i++){
+            hour_wrapper.classList.remove(`digit-${i}`);
+            ten_minute_wrapper.classList.remove(`digit-ten-${i}`);
+            minute_wrapper.classList.remove(`digit-${i}`);
+            ten_second_wrapper.classList.remove(`digit-ten-${i}`);
+            second_wrapper.classList.remove(`digit-${i}`);
+        }
+        
+        hour_wrapper.classList.add(`digit-${hours}`);
+        if(ten_minutes == 0){
+            if(!ten_minute_wrapper.classList.contains(`digit-ten-0-start`)){
+                ten_minute_wrapper.classList.add(`digit-ten-0-end`);
+                setTimeout(e => {
+                    ten_minute_wrapper.classList.remove(`digit-ten-0-end`);
+                    ten_minute_wrapper.classList.add(`digit-ten-0-start`);
+                }, 500);
+            }
+        }else{
+            ten_minute_wrapper.classList.remove(`digit-ten-0-start`);
+            ten_minute_wrapper.classList.add(`digit-ten-${ten_minutes}`);
+        }
+        if(minutes == 0){
+            if(!minute_wrapper.classList.contains(`digit-0-start`)){
+                minute_wrapper.classList.add(`digit-0-end`);
+                setTimeout(e => {
+                    minute_wrapper.classList.remove(`digit-0-end`);
+                    minute_wrapper.classList.add(`digit-0-start`);
+                }, 500);
+            }
+        }else{
+            minute_wrapper.classList.remove(`digit-0-start`);
+            minute_wrapper.classList.add(`digit-${minutes}`);
+        }
+        if(ten_seconds == 0){
+            if(!ten_second_wrapper.classList.contains(`digit-ten-0-start`)){
+                ten_second_wrapper.classList.add(`digit-ten-0-end`);
+                setTimeout(e => {
+                    ten_second_wrapper.classList.remove(`digit-ten-0-end`);
+                    ten_second_wrapper.classList.add(`digit-ten-0-start`);
+                }, 500);
+            }
+        }else{
+            ten_second_wrapper.classList.remove(`digit-ten-0-start`);
+            ten_second_wrapper.classList.add(`digit-ten-${ten_seconds}`);
+        }
+        if(seconds == 0){
+            if(!second_wrapper.classList.contains(`digit-0-start`)){
+                second_wrapper.classList.add(`digit-0-end`);
+                setTimeout(e => {
+                    second_wrapper.classList.remove(`digit-0-end`);
+                    second_wrapper.classList.add(`digit-0-start`);
+                }, 500);
+            }
+        }else{
+            second_wrapper.classList.remove(`digit-0-start`);
+            second_wrapper.classList.add(`digit-${seconds}`);
+        }
+    }
+
+
+
+
+
+
+
+}
+
+update();
