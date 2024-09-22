0fd235a8d15339f0cd64b927628532601305e3e0
PinkAntEaterMysipasi <rostislav.kozlik@seznam.cz>
2024-09-22 14:21:13 +0200

    ws fix

--- src/ws.go
+++ src/ws.go
@@ -118,10 +118,10 @@ func PlayWsEndpoint(dao *daos.Dao) echo.HandlerFunc {
     log.Info(c.Request().URL)
 
     teamChanMapMutex.Lock()
-
     teamchan, ok := TeamChanMap[teamid]
     if !ok {
       teamchan = &TeamChanMu{sync.RWMutex{}, make([]chan string, 5, 5)}
+      log.Info(teamchan)
       TeamChanMap[teamid] = teamchan
     }
     teamChanMapMutex.Unlock()
