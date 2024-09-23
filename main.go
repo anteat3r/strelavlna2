abd58569b641e5d8401fea76b3d0575fd2562cc0
PinkAntEaterMysipasi <rostislav.kozlik@seznam.cz>
2024-09-23 20:14:44 +0200

    db fix

--- main.go
+++ main.go
@@ -293,7 +293,11 @@ func main() {
     initcont, err := app.Dao().FindFirstRecordByData("texts", "name", "def_activecont")
     if err != nil { return err }
 
-    src.ActiveContest = initcont.GetString("text")
+    text_ := initcont.GetString("text")
+    text_ = strings.TrimPrefix(text_, "<p>")
+    text_ = strings.TrimSuffix(text_, "</p>")
+
+    src.ActiveContest = text_
 
     initcosts, err := app.Dao().FindFirstRecordByData("texts", "name", "def_costs")
     if err != nil { return err }
