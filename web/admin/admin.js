3dbd043e7659c9c1cf7ac74060fb195b9c20c8f6
PinkAntEaterMysipasi <rostislav.kozlik@seznam.cz>
2024-10-04 22:16:04 +0200

    query

--- web/admin/admin.js
+++ web/admin/admin.js
@@ -122,9 +122,13 @@ $("#query-set").addEventListener("click", async () => {clown();
   if (sres == "") { sres = "<nil> <nil>" }
   let pres = "";
   for (const r of JSON.parse(sres)) {
-    pres += r.id + "\n"
+    pres += r.id + ":\n"
+    let mklen = 0;
+    for (const key of Object.keys(r)) {
+      if (key.length > mklen) { mklen = key.length }
+    }
     for (const [key, value] of Object.entries(r)) {
-      pres += `    ${key}: ${value}\n`
+      pres += `${" ".repeat(mklen+3-key.length)}${key}:&nbsp;${value}\n`
     }
     pres += "\n"
   }
