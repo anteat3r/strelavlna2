05f70fb08abcd758e945f9376f67a3be0254e86e
PinkAntEaterMysipasi <rostislav.kozlik@seznam.cz>
2024-10-05 17:02:22 +0200

    query

--- web/admin/admin.js
+++ web/admin/admin.js
@@ -128,7 +128,7 @@ $("#query-set").addEventListener("click", async () => {clown();
       if (key.length > mklen) { mklen = key.length }
     }
     for (const [key, value] of Object.entries(r)) {
-      pres += `${"&nbsp;".repeat(mklen+3-key.length)}${key}:&nbsp;${value}<br>`
+      pres += `${"&nbsp;".repeat(mklen+3-key.length)}<span style="color: yellow;">${key}</span>:&nbsp;${value}<br>`
     }
     pres += "<br>"
   }
