134406afe9baa01a67ee7e724ba6ae38cac5c11d
PinkAntEaterMysipasi <rostislav.kozlik@seznam.cz>
2024-11-15 20:41:46 +0100

    admin

--- web/admin/admin.js
+++ web/admin/admin.js
@@ -150,19 +150,23 @@ $("#query-set").addEventListener("click", async () => {clown();
     {headers: {"Authorization": pb.authStore.token},
   })
   let sres = await res.text();
+  if (!res.ok) {
+    alert(sres);
+    return;
+  }
   if (sres == "") { sres = "<nil> <nil>" }
   let pres = "";
   for (const r of JSON.parse(sres)) {
-    pres += `<span style="color: blue;">` + r.id + ":</span><br>"
+    // pres += `<span style="color: blue;">` + r.id + ":</span><br>"
     let mklen = 0;
     for (const key of Object.keys(r)) {
       if (key.length > mklen) { mklen = key.length }
     }
     for (const [key, value] of Object.entries(r)) {
       if (value.length > 50) {
-        pres += `${"&nbsp;".repeat(mklen+3-key.length)}<span style="color: green;">${key}:</span>&nbsp;<span id="dots-${r.id}-${key}">...</span><br>`
+        pres += `${"&nbsp;".repeat(mklen-key.length)}<span style="color: green;">${key}:</span>&nbsp;<span id="dots-${r.id}-${key}">...</span><br>`
       } else {
-        pres += `${"&nbsp;".repeat(mklen+3-key.length)}<span style="color: green;">${key}:</span>&nbsp;${value}<br>`
+        pres += `${"&nbsp;".repeat(mklen-key.length)}<span style="color: green;">${key}:</span>&nbsp;${value}<br>`
       }
     }
     pres += "<br>"
