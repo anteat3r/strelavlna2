6dd0e755c506f8dac2faaca0587af4185bf953dd
PinkAntEaterMysipasi <rostislav.kozlik@seznam.cz>
2024-11-16 17:21:01 +0100

    autogen

--- src/grapheditor.go
+++ src/grapheditor.go
@@ -3,6 +3,8 @@ package src
 import (
 	"encoding/json"
 	"math/rand/v2"
+	"strconv"
+	"strings"
 )
 
 type GraphNode interface {
@@ -10,7 +12,7 @@ type GraphNode interface {
 }
 
 type Graph map[GraphId]GraphNode
-type PathSet map[GraphId]struct{}
+type PathSet map[GraphId]any
 type GraphId = int
 
 type InvalidGraphErr struct { msg string }
@@ -42,6 +44,13 @@ func ParseGraph(graphs string) (Graph, error) {
       if !ok { return nil, InvalidGraphErr{"invalid node inp"} }
       pnode = AdditionNode[int]{GraphId(id), ([]GraphId)(inps)}
 
+    case "startfloat":
+      inp, ok := nd["input"].(int)
+      if !ok { return nil, InvalidGraphErr{"invalid node inp"} }
+      name, ok := nd["value"].(string)
+      if !ok { return nil, InvalidGraphErr{"invalid node inp"} }
+      pnode = StartNode[float64]{name, GraphId(inp)}
+
     default:
       return nil, InvalidGraphErr{"invalid node type"} 
 
@@ -51,16 +60,58 @@ func ParseGraph(graphs string) (Graph, error) {
   return graphres, nil
 }
 
-func (g Graph) Compute(start int) (any, error) {
-  nd, ok := g[GraphId(start)]
-  if !ok { return nil, InvalidGraphErr{"start not found"} }
-  return nd.Compute(g, make(PathSet))
+func (g Graph) Generate(text, sol string) (string, string, error) {
+  cache := make(PathSet)
+  for _, nd := range g {
+    strnd, ok := nd.(StartNode[string])
+    if ok {
+      ndres, err := strnd.Compute(g, cache)
+      if err != nil { return "", "", err }
+      ndresstr, ok := ndres.(string)
+      if !ok { return "", "", InvalidGraphErr{"invalid ret type"} }
+      text = strings.ReplaceAll(text, "`" + strnd.name + "`", ndresstr)
+      sol = strings.ReplaceAll(sol, "`" + strnd.name + "`", ndresstr)
+      continue
+    }
+    fltnd, ok := nd.(StartNode[float64])
+    if ok {
+      ndres, err := fltnd.Compute(g, cache)
+      if err != nil { return "", "", err }
+      ndresflt, ok := ndres.(float64)
+      if !ok { return "", "", InvalidGraphErr{"invalid ret type"} }
+      ndresstr := strconv.FormatFloat(ndresflt, 'g', -1, 64)
+      text = strings.ReplaceAll(text, "`" + strnd.name + "`", ndresstr)
+      sol = strings.ReplaceAll(sol, "`" + strnd.name + "`", ndresstr)
+      continue
+    }
+  }
+  return text, sol, nil
 }
 
 type LiteralNode struct{ val any }
 var _ GraphNode = (*LiteralNode)(nil)
 func (v LiteralNode) Compute(Graph, PathSet) (any, error) {
   return v.val, nil
+}
+
+type StartNode[T float64 | string] struct{
+  name string
+  inp GraphId
+}
+var _ GraphNode = (*StartNode[float64])(nil)
+func (v StartNode[T]) Compute(g Graph, cache PathSet) (any, error) {
+  cval, ok := cache[v.inp]
+  if ok {
+    if cval == nil { return nil, InvalidGraphErr{"cyclic ref"} }
+    return cval, nil
+  }
+  nd, ok := g[v.inp]
+  if !ok { return nil, InvalidGraphErr{"invalid ref"} }
+  comp, err := nd.Compute(g, cache)
+  if err != nil { return nil, err }
+  compt, ok := comp.(T)
+  if !ok { return nil, InvalidGraphErr{"invalid type"} }
+  return compt, nil
 }
 
 type AdditionNode[T int | float64] struct {
@@ -70,7 +121,11 @@ type AdditionNode[T int | float64] struct {
 var _ GraphNode = (*AdditionNode[int])(nil)
 var _ GraphNode = (*AdditionNode[float64])(nil)
 func (v AdditionNode[T]) Compute(g Graph, cache PathSet) (any, error) {
-  if _, ok := cache[v.id]; ok { return nil, InvalidGraphErr{"cyclic ref"} }
+  cval, ok := cache[v.id]
+  if ok {
+    if cval == nil { return nil, InvalidGraphErr{"cyclic ref"} }
+    return cval, nil
+  }
   var res T
   cache[v.id] = struct{}{}
   for _, inp := range v.inputs {
@@ -82,7 +137,7 @@ func (v AdditionNode[T]) Compute(g Graph, cache PathSet) (any, error) {
     if !ok { return nil, InvalidGraphErr{"invalid type"} }
     res += compt
   }
-  delete(cache, v.id)
+  cache[v.id] = res
   return res, nil
 }
 
