0b39a93fb046cf74b71d05826781e04140796dd8
PinkAntEaterMysipasi <rostislav.kozlik@seznam.cz>
2024-11-16 18:00:12 +0100

    ruleset_update

--- web/probeditor/graph_funcs.js
+++ web/probeditor/graph_funcs.js
@@ -1,70 +1,26 @@
-function addition(i, v, t){
-    return i[0] + i[1];
-}
-function subtraction(i, v, t){
-    return i[0] - i[1];
-}
-function multiplication(i, v, t){
-    return i[0] * i[1];
-}
-function division(i, v, t){
-    return i[0] / i[1];
-}
-function tostring(i, v, t){
-    return i.toString();
-}
-function fromstring(i, v, t){
-    return parseFloat(i);
-}
-function randomInteger(i, v, t){
-    return Math.floor(Math.random() * i[0]) + i[1];
-}
-function randomFloat(i, v, t){
-    return Math.random() * i[0] + i[1];
-}
-function number(i, v, t){
-    return v;
-}
-function string(i, v, t){
-    return v;
-}
-function constant(i, v, t){
-    return parseFloat(t.find(t => t.id == i[0]).value);
-}
-
-function setnumber(i, v, t){
-    return i[0];
-}
-function setstring(i, v, t){
-    return i[0];
-}
-function round(i, v, t){
-    return Math.round(i[0]);
-}
-function roundplaces(i, v, t){
-    return parseFloat(i[0].toFixed(i[1]));
-}
-
 export default {
-    "addition": addition,
-    "subtraction": subtraction,
-    "multiplication": multiplication,
-    "division": division,
+    "addition": (i, _, __) => i[0] + i[1],
+    "subtraction": (i, _, __) => i[0] - i[1],
+    "multiplication": (i, _, __) => i[0] * i[1],
+    "division": (i, _, __) => i[0] / i[1],
 
-    "tostring": tostring,
-    "fromstring": fromstring,
+    "tostring": (i, _, __) => i.toString(),
+    "fromstring": (i, _, __) => parseFloat(i),
 
-    "randomInteger": randomInteger,
-    "randomFloat": randomFloat,
+    "randomInteger": (i, _, __) => Math.floor(Math.random()*(i[0]-i[1])+i[1]),
+    "randomFloat": (i, _, __) => Math.random()*(i[0]-i[1])+i[1],
 
-    "constant": constant,
+    "constant": (i, _, t) => parseFloat(t.find(t => t.id == i[0]).value),
 
-    "string": string,
-    "setstring": setstring,
-    "number": number,
-    "setnumber": setnumber,
+    "string": (_, v, __) => v,
+    "setstring": (i, _, __) => i[0],
+    "number": (_, v, __) => v,
+    "setnumber": (i, _, __) => i[0],
 
-    "round": round,
-    "roundplaces": roundplaces,
+    "round": (i, _, __) => Math.round(i[0]),
+    "roundplaces": (i, _, __) => parseFloat(i[0].toFixed(i[1])),
+
     "numbercomparison": (i, _, __) => i[0] == i[1],
+    "numberif": (i, _, __) => i[0] ? i[1] : i[2],
+    "stringif": (i, _, __) => i[0] ? i[1] : i[2],
 }
