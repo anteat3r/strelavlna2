9ee08ea3a701248fd676e6f04eb9fb80f6ac36af
PinkAntEaterMysipasi <rostislav.kozlik@seznam.cz>
2024-11-19 09:37:37 +0100

    fix

--- src/db.go
+++ src/db.go
@@ -3,9 +3,10 @@ package src
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io/fs"
 	"os"
-	"slices"
+	// "slices"
 	"sort"
 	"strings"
 	"sync"
@@ -1270,31 +1271,45 @@ func compileSectors(probs []string, sectors [][][]string, admins []string) [][]s
 }
 
 func DBGenProbWorkers(probsr map[string]ProbM) error {
-  corrs, err := App.Dao().FindRecordsByFilter(
-    "correctors",
-    `username != ""`,
-    "-created", 0, 0,
-  )
-  if err != nil { return err }
-  admins := make([]string, len(corrs))
-  for i, corr := range corrs {
-    admins[i] = corr.GetId()
-  }
-  probs := make([]string, 0, len(probsr))
-  for id, _ := range probsr {
-    probs = append(probs, id)
-  }
-  sectors := make([][][]string, 1)
-  sectors[0] = make([][]string, 0)
-  for range admins {
-    sectors[0] = append(sectors[0], make([]string, 0))
-  }
-  for id, pr := range probsr {
-    pr.RWith(func(v ProbS) {
-      idx := slices.Index(admins, v.Author)
-      sectors[0][idx] = append(sectors[0][idx], id)
-    })
-  }
+  // corrs, err := App.Dao().FindRecordsByFilter(
+  //   "correctors",
+  //   `username != ""`,
+  //   "-created", 0, 0,
+  // )
+  // if err != nil { return err }
+  // admins := make([]string, len(corrs))
+  // for i, corr := range corrs {
+  //   admins[i] = corr.GetId()
+  // }
+  // probs := make([]string, 0, len(probsr))
+  // for id, _ := range probsr {
+  //   probs = append(probs, id)
+  // }
+  // sectors := make([][][]string, 1)
+  // sectors[0] = make([][]string, 0)
+  // for range admins {
+  //   sectors[0] = append(sectors[0], make([]string, 0))
+  // }
+  // for id, pr := range probsr {
+  //   pr.RWith(func(v ProbS) {
+  //     idx := slices.Index(admins, v.Author)
+  //     sectors[0][idx] = append(sectors[0][idx], id)
+  //   })
+  // }
+var admins = []string{"a", "b", "c", "d", "e", "f", "g", "h"}
+var probs = func() []string {
+	problems := make([]string, 500)
+	for i := range problems {
+		problems[i] = fmt.Sprintf("u%d", i)
+	}
+	return problems
+}()
+var sectors = [][][]string{
+	{
+		{"u0", "u1"}, {"u2", "u3", "u4"}, {"u5"}, {"u6", "u7"},
+		{"u8", "u9"}, {"u10", "u11", "u12"}, {"u13", "u14", "u15"}, {"u16", "u17", "u18", "u19"},
+	},
+}
   log.Info(sectors, probs, admins)
 
   for newSector(probs, sectors, admins) {}
