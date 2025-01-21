ffa94699e3909e4353cd02a30fde7f28f0c2a192
PinkAntEaterMysipasi <rostislav.kozlik@seznam.cz>
2025-01-21 09:58:15 +0100

    dashboard

--- web/admin/admin.js
+++ web/admin/admin.js
@@ -294,6 +294,7 @@ $("#dashboard-s").addEventListener("click", async () => {
     let arr = Array.from(dashboard.values());
     arr.sort((a, b) => b.score - a.score)
     for (let [idx, it] of arr.entries()) {
+      console.log(idx, it);
       sres += `<li>${idx+1}. ${it.name}:${" ".repeat(30-it.name.length)}${it.score}</li>`;
     }
     console.log(sres);
